9fcd53b7557a1b74b90fbd6cdbedf4988dde6ee9
reggiemcdonald <regmcdonald95@gmail.com>
2019-06-21 10:29:32 -0700

    Initial Commit

---
+++ ToDos/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDos
+//
+//  Created by Reginald McDonald on 2019-06-21.
+//  Copyright © 2019 Reginald McDonald. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDos/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ToDos
+//
+//  Created by Reginald McDonald on 2019-06-21.
+//  Copyright © 2019 Reginald McDonald. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
